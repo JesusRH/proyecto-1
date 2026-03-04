cafd9c71254a2dd7a398d22eb6c0a741846e7780
Jesus Reyes <jesuselygyo@gmail.com>
2026-03-04 01:59:24 -0600

    formulario logging y alert de error

--- src/app/pages/loggin/loggin.ts
+++ src/app/pages/loggin/loggin.ts
@@ -1,16 +1,40 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-loggin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './loggin.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Loggin implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+
+  fb = inject(FormBuilder)
+  hasError= signal(false)
+  isPosting= signal(false)
+
+  loggingForm = this.fb.group({
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+
+  });
+
+  onSubmit(){
+    if(this.loggingForm.invalid){
+      this.hasError.set(true)
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+    const{ email='', password =''}= this.loggingForm.value;
+    console.log({email, password});
+  }
+
 
 
   ngOnInit(): void {
